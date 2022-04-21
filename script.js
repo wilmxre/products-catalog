f0c51d60b18a23e1eeadd99a44bd705d0230d340
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-21 12:14:08 +0300

    Polish design

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-const middle = document.querySelector('.middle-section');
+const slides = document.querySelector('.slides');
 
 for (let i = 0; i < 3; i++) {
+  const link = document.createElement('a');
+  link.href = 'http://www.google.com';
+
   const slide = document.createElement('div');
   slide.classList.add('slide');
-  middle.appendChild(slide);
+  slide.setAttribute('id', `slide-${i}`);
+  link.appendChild(slide);
+  slides.appendChild(link);
 
   const slideTitle = document.createElement('div');
   slideTitle.classList.add('slide-title');
   slideTitle.textContent = 'Velux';
   slide.appendChild(slideTitle);
+  link.appendChild(slide);
+  slides.appendChild(link);
 
   const slideImage = document.createElement('img');
   slideImage.src = 'https://i.picsum.photos/id/1003/1181/1772.jpg?hmac=oN9fHMXiqe9Zq2RM6XT-RVZkojgPnECWwyEF1RvvTZk';
   slideImage.classList.add('slide-img');
   slide.appendChild(slideImage);
+  link.appendChild(slide);
+  slides.appendChild(link);
 }
 
 
